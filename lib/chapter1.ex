6f267e81a0c920bda1650dae4b1218981479d215
Kevin Kim <kevin@coconutcalendar.com>
2017-03-16 23:18:24 -0600

    chapter 1 complete

--- lib/chapter1.ex
+++ lib/chapter1.ex
@@ -1,6 +1,6 @@
-defmodule Littel.Recursive do
-  def fac n when n == do, do: 1
-  def fac n when n > 0 do
+defmodule Little.Recursive do
+  def fac(n) when n == 0, do: 1
+  def fac(n) when n > 0 do
     n * fac(n-1)
   end
 end
