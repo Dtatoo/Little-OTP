fd0bddbf13a1699e1e94d8e48fd60c9e0c0ea4f7
Kevin Kim <jok068@mail.usask.ca>
2017-04-17 08:30:56 -0600

    added assertion to the test
    This makes sure app process is restarted every time in each test

--- apps/pooly/test/pooly_test.exs
+++ apps/pooly/test/pooly_test.exs
@@ -3,13 +3,19 @@ defmodule PoolyTest do
   alias Pooly
 
   setup do
-    {:ok, sup_pid} = Pooly.start([],[])
-    {:ok, %{sup_pid: sup_pid}}
+    {:ok, pid} = Pooly.start([],[])
+
+    on_exit(fn ->
+      Application.stop(:pooly)
+      Application.unload(:pooly)
+      assert_down(pid)
+    end)
+    {:ok, %{app_pid: pid}}
 
   end
 
   describe "Pooly.start/2" do
-    test "can start a supervisor process", %{sup_pid: pid} do
+    test "can start a supervisor process", %{app_pid: pid} do
       assert Process.alive?(pid)
     end
 
@@ -36,9 +42,15 @@ defmodule PoolyTest do
 
   end
 
-  describe "Pooly.checkin/1" do
-    test "can return worker pid" do
-    end
+  # describe "Pooly.checkin/1" do
+  #   # test "can return worker pid" do
+  #   # end
+  # end
+
+  defp assert_down(pid) do
+    ref = Process.monitor(pid)
+    assert_receive {:DOWN, ^ref, _, _, _}
   end
 
 end
+
